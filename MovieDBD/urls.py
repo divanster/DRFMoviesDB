10680a8b6496b71314eb892f07a53e191624bfca
divanster <divanster@gmail.com>
2024-04-25 17:00:34 +0300

    adding adding image field in model done working

--- MovieDBD/urls.py
+++ MovieDBD/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from movies.views import MovieViewSet, ComedyViewSet, ActionViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register('movies', MovieViewSet)
@@ -25,6 +27,6 @@ router.register('comedy', ComedyViewSet, basename='comedy')
 router.register('action', ActionViewSet, basename='action')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-]
+                  path('', include(router.urls)),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- movies/serilizers.py
+++ movies/serilizers.py
@@ -3,6 +3,9 @@ from .models import Moviedata
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    # image = serializers.ImageField(max_length=None, allow_empty_file=False, required=False, use_url=True)
+    # This can be added if fields are not ALL, if you specified separate fiels. Its variable, must be put into fields...
+
     class Meta:
         model = Moviedata
         fields = '__all__'
